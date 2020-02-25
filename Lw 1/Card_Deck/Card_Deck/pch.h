79a11bbe43d5928843018e99097eec0a8441ab65
MaxMartST <AdderGtx@gmail.com>
2020-02-25 15:45:12 +0300

    обновил реп

--- Lw 1/Card_Deck/Card_Deck/pch.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <random>
-#include <algorithm>
-#include <iterator>
-#include <iostream>
-
-void PrintCardDeck(std::vector<int> myV);
-void ShuffleCardDeck(std::vector<int> &myV, std::mt19937 myG);
